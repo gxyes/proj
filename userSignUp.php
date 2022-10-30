be004db9d657fb94753c5e536ccf43241ef9ef13
Xuefuguoalita <xuefuguoalita@gmail.com>
2022-10-30 23:52:09 +0800

    All in Eng; new user has avatar now

--- userSignUp.php
+++ userSignUp.php
@@ -32,7 +32,7 @@
         // echo '<script>alert("Finish! Please sign in!")</script>';
         // sleep(3);
         // header('location:login.php');
-        $add_user_sql = "insert into users values ('$user', '$pwd', '')";
+        $add_user_sql = "insert into users values ('$user', '$pwd', 'https://images.unsplash.com/photo-1667120651808-34305af680d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60')";
         $add_result = $db->query($add_user_sql);
         echo '<script>alert("Finish! Please sign in!"); location.href="login.php"</script>';
     }
